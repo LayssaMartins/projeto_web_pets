05c2b8bc49961876f98a02030c5d6cbbd707acfd
LayssaMartins <layssamartins12@gmail.com>
2025-11-13 19:51:03 -0300

    Primeiro commit do projeto

---
+++ includes/rodape.php
@@ -0,0 +1,10 @@
+  <footer>
+    <p>© 2025 Peludos Unipê 🐾 | Projeto desenvolvido por 
+      <strong>Layssa Evellyn</strong> e <strong>Tainara David</strong> — 
+      Centro Universitário de João Pessoa.
+    </p>
+  </footer>
+  <script src="../assets/js/patinhas.js"></script>
+
+</body>
+</html>
---
+++ pages/index.php
@@ -0,0 +1,29 @@
+<?php include('../includes/menu.php'); ?>
+<link rel="stylesheet" href="../assets/css/style.css">
+
+<main class="home">
+  <section class="hero">
+    <h1>Adote Amor, Adote um Peludo 🐶🐱</h1>
+    <p>Transforme a vida de um animal abandonado e receba amor incondicional em troca. <br>No <strong>Peludos Unipê</strong>, cada adoção é uma oportunidade de fazer o mundo mais feliz para nossos amigos de quatro patas. 🏡💛</p>
+    <a href="sobre.php" class="btn">Quero Saber mais</a>
+  </section>
+
+<section class="">
+  <h2>Conheça Alguns Animais:</h2>
+  <div class="carousel">
+    <div class="carousel-track">
+      <div class="carousel-item"><img src="../assets/img/peludo1.png" alt="Peludo 1"></div>
+      <div class="carousel-item"><img src="../assets/img/peludo2.png" alt="Peludo 2"></div>
+      <div class="carousel-item"><img src="../assets/img/peludo3.png" alt="Peludo 3"></div>
+      <div class="carousel-item"><img src="../assets/img/peuludo4.png" alt="Peuludo 4"></div>
+      <!-- duplicando para loop infinito -->
+      <div class="carousel-item"><img src="../assets/img/peludo1.png" alt="Peludo 1"></div>
+      <div class="carousel-item"><img src="../assets/img/peludo2.png" alt="Peludo 2"></div>
+      <div class="carousel-item"><img src="../assets/img/peludo3.png" alt="Peludo 3"></div>
+      <div class="carousel-item"><img src="../assets/img/peuludo4.png" alt="Peuludo 4"></div>
+    </div>
+  </div>
+</section>
+
+</main>
+<?php include('../includes/rodape.php'); ?>
